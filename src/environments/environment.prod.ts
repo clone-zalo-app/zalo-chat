a40cf5ff9412584569ed7a80ad0eb770aeb468e1
leduclinh-it <leduclinh65@gmail.com>
2020-11-09 22:57:24 +0700

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  userUrl: 'http://18.188.96.23:3009/',
-  chatUrl: 'http://18.188.96.23:3007/',
+  userUrl: 'http://52.221.226.206:3009/',
+  chatUrl: 'http://52.221.226.206:3007/',
   firebaseConfig: {
     apiKey: "AIzaSyBXz2-PZTw9k3dbs_FmBbOI3Qr04s67bG4",
     authDomain: "zalo-app-32088.firebaseapp.com",
