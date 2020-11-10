590f7c661851c5dea0f3af004f82467fefddaf8d
leduclinh-it <leduclinh65@gmail.com>
2020-11-10 11:33:45 +0700

    validate web

---
+++ src/app/shared/components/alert-system/alert-system.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alert-system',
+  templateUrl: './alert-system.component.html',
+  styleUrls: ['./alert-system.component.scss']
+})
+export class AlertSystemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,18 +4,20 @@ import { NotFoundComponent } from './components/not-found/not-found.component';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import { ModalUpdateProfileComponent } from './components/modal-update-profile/modal-update-profile.component';
+import { AlertSystemComponent } from './components/alert-system/alert-system.component';
 
 @NgModule({
-  declarations: [NotFoundComponent, ModalUpdateProfileComponent],
+  declarations: [NotFoundComponent, ModalUpdateProfileComponent, AlertSystemComponent],
   imports: [
     MDBBootstrapModule.forRoot(),
     CommonModule,
     FormsModule
   ],
-  exports: [
-    MDBBootstrapModule,
-    CommonModule,
-    FormsModule
-  ]
+    exports: [
+        MDBBootstrapModule,
+        CommonModule,
+        FormsModule,
+        AlertSystemComponent
+    ]
 })
 export class SharedModule {}
